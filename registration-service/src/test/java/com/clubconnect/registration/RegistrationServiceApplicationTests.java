efb6170556a8416505b762575a47eb1141b1558c
unknown <sohamvashists8@gmail.com>
2025-08-12 23:43:51 +0530

    main

---
+++ registration-service/src/test/java/com/clubconnect/registration/RegistrationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clubconnect.registration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
